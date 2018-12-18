61e5ca9d7dc672670054c0380077af3b85dcb433
Felipe Girotti <felipe.girotti@gmail.com>
2018-12-17 22:39:15 -0200

    add client with timeout

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +15,16 @@ public class DemoController {
 
     RestOperations restOperations = new RestTemplate();
 
+    @Autowired
+    RestOperations client;
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Map getData(@PathVariable long id) {
         return restOperations.getForObject("http://localhost:8999/rest/" + id, Map.class);
     }
 
+    @GetMapping(value = "/slow", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Map getDataSlow() {
+        return  client.getForObject("http://localhost:8999/rest/2",  Map.class);
+    }
 }
